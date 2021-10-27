329b4a3652933d1caa6714a92b04a9a3cfe12950
Ergash0 <92915363+Ergash0@users.noreply.github.com>
2021-10-27 13:42:56 +0600

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <pre><h1 syle="text-align: center;">                                         my Portofolio:
 <pre><h2>Name:  Nurik</h2></pre>
 <pre><h2>Info: I live in Bishkek. 17 years old</h2></pre>                  
-<pre><h2>Phone number: 0558559989</h2></pre>
+<pre><h2>Phone number: 0709491919</h2></pre>
 <img src="index.jpg" alt="">
 
 <pre><h1;">
