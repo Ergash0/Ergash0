b258be4b710d9a932a5e5b462fd86e9c5aa6b093
Ergash0 <92915363+Ergash0@users.noreply.github.com>
2021-10-27 14:10:41 +0600

    Update index.php

--- index.php
+++ index.php
@@ -23,3 +23,12 @@ i study in kolledj Alatoo</h2></pre>
 <pre><h1;">
 </body>
 </html>
+<html>
+<body>
+
+<h2>HTML Image</h2>
+<img src="pic_trulli.jpg" alt="Trulli" width="500" height="333">
+
+</body>
+</html>
+
